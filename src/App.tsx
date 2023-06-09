35881f5e8a5b427c41e7aa4d720d3a3d6a7a63cd
nischalmudennavar <nischalmudennavar@gmail.com>
2023-06-09 12:14:08 +0530

    notification badge

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.scss";
 import Footer from "./components/Footer/Footer";
+import Notification from "./components/Notification/Notification";
 
 
 function App() {
   return <div className="app">
+    <Notification/>
     <Footer/>
   </div>;
 
