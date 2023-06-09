d5da5c40bb98b0cdce41782519ee187638c377ea
nischalmudennavar <nischalmudennavar@gmail.com>
2023-06-09 20:07:25 +0530

    added news widget

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.scss";
 import Footer from "./components/Footer/Footer";
+import News from "./components/News/News";
 import Notification from "./components/Notification/Notification";
 
 
 function App() {
   return <div className="app">
+    <News/> 
     <Notification/>
     <Footer/>
   </div>;
