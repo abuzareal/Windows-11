1a1148911e6b220a20511028ef6fd0054581aa7c
Abuzar <abuzart1999@gmail.com>
2023-06-13 17:07:51 +0530

    News Bug

--- src/components/News/News.tsx
+++ src/components/News/News.tsx
@@ -1,33 +1,33 @@
 import { useEffect, useState } from "react";
 import "./News.scss";
-import axios from "axios";
 import { gif } from "../../data/images";
+import axios from "axios";
 
 const News = () => {
-  const [news, setNews] = useState([]);
+  const [news, setNews] = useState<News[]>([]);
   const [isLoading, setIsLoading] = useState(true);
+
+  type News = {
+    id: string;
+    name: string;
+    url: string;
+    description: string;
+  };
 
   useEffect(() => {
     const fetchNews = async () => {
-      const options = {
-        method: "GET",
-        url: "https://newsdata2.p.rapidapi.com/sources",
-        params: {
-          language: "en",
-          country: "US",
-          category: "Technology",
-        },
-        headers: {
-          "X-RapidAPI-Key":
-            "4e8a29ced8msh70ccb8b4c63f94ep157a4cjsnddad9329e7ab",
-          "X-RapidAPI-Host": "newsdata2.p.rapidapi.com",
-        },
-      };
+      const url =
+        "https://api.newsdata.io/v1/news?apikey=YOUR_API_KEY&q=technology";
 
       try {
-        const response = fetch(options);
+        const response = await axios.get<News[]>(url, {
+          headers: {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+          },
+        });
         console.log(response.data);
-        setNews(response.data.results);
+        setNews(response.data);
         setIsLoading(false); // Set loading state to false after fetching news
       } catch (error) {
         console.error(error);
@@ -39,40 +39,38 @@ const News = () => {
   }, []);
 
   return (
-    <>
-      <div className="news-container">
-        <h2>News Feed 📰</h2>
-        <hr />
-        {isLoading ? (
-          <p>
-            <img
-              src={gif}
-              alt=""
-              style={{
-                width: "100px",
-              }}
-            />
-          </p> // Render the loader when isLoading is true
-        ) : news.length > 0 ? (
-          news.map((newsItem: any) => (
-            <div className="news-item" key={newsItem.id}>
-              <h3>
-                <span id="title">{newsItem.name}</span>
-                <span id="author">{newsItem.id}</span>
-              </h3>
-              <hr />
-              <p>{newsItem.description}</p>
-              <a href={newsItem.url} target="_blank" rel="noreferrer">
-                Read More
-              </a>
-            </div>
-          ))
-        ) : (
-          <p>There are no news</p>
-        )}
-        <div className="news-footer-shadow"></div>
-      </div>
-    </>
+    <div className="news-container">
+      <h2>News Feed 📰</h2>
+      <hr />
+      {isLoading ? (
+        <p>
+          <img
+            src={gif}
+            alt=""
+            style={{
+              width: "100px",
+            }}
+          />
+        </p>
+      ) : news.length > 0 ? (
+        news.map((newsItem) => (
+          <div className="news-item" key={newsItem.id}>
+            <h3>
+              <span id="title">{newsItem.name}</span>
+              <span id="author">{newsItem.id}</span>
+            </h3>
+            <hr />
+            <p>{newsItem.description}</p>
+            <a href={newsItem.url} target="_blank" rel="noreferrer">
+              Read More
+            </a>
+          </div>
+        ))
+      ) : (
+        <p>There are no news</p>
+      )}
+      <div className="news-footer-shadow"></div>
+    </div>
   );
 };
 
